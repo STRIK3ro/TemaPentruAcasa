400041afe898a6b6bfd44ec7e08b9acbd06a71cb
STRIK3ro <STRIK3.2014@yahoo.com>
2020-05-03 16:46:57 +0300

    Tema

--- 3MaiTema1/src/Main.java
+++ 3MaiTema1/src/Main.java
@@ -1,7 +1,4 @@
 public class Main {
-    //Cerinta
-    //O functie care primeste ca 2 strigrui ca parametru si verifica daca al doulea string exista in primul string
-
     public static void main(String[] args) {
         String string1="Ana are pere de aceasta data.";
         String string2="Ana are pere";
@@ -11,10 +8,11 @@ public class Main {
 
     }
     public static void verificaIncludere(String string1,String string2){
-    if(string1.contains(string2)){
-        System.out.println("Primul il contine pe al doilea");
-    }else{
-        System.out.println("Primul string nu il contine pe al doilea");
+        if(string1.contains(string2)){
+            System.out.println("Primul il contine pe al doilea");
+        }else{
+            System.out.println("Primul string nu il contine pe al doilea");
+        }
     }
     }
-}
+
